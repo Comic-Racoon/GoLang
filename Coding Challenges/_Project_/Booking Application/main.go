6e55cac5efdbbb4038ae5cb06b11902c0cf850ca
Rahul Mhatre <67099690+Comic-Racoon@users.noreply.github.com>
2023-02-19 16:28:06 +0530

    added git ignore and truing to ignre .idea

--- Coding Challenges/_Project_/Booking Application/main.go
+++ Coding Challenges/_Project_/Booking Application/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	//	greet the customers and provide information about tickets available and print them in cli
+
+	var conferenceName = "Go Lang Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to ", conferenceName, " Booking Application")
+	fmt.Printf("We have total of %v  tickets  and %v tikets are still available. \n", conferenceTickets, remainingTickets)
+	fmt.Printf("Get your tickets to attend")
+
 }
